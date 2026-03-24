197b965c2beb9febab17ea8bd595c2274deefb75
Hamza Barchan <keckhamza@gmail.com>
2026-03-24 22:27:07 +0100

    Revert "feat: update UserRoles enum methods for clarity and add Dockerfile for environment setup"
    This reverts commit 8dad6b65359e4c7fe9e0199312cb241d2532ff07.

--- app/Enums/UserRoles.php
+++ app/Enums/UserRoles.php
@@ -1,30 +1,25 @@
 <?php
-
 namespace App\Enums;
-
-enum UserRoles: int
+enum UserRoles: int{
+case DOCTOR     = 0;
+case SECRETARY  = 1;
+case ADMIN      = 2;
+public static function values(): array
+    {
+         return array_column(self::cases(), 'value','name');
+        // ["deposit" => "Deposit", "withdraw" => "Withdraw"]
+    }
+public static  function isDoctor( UserRoles $useRole)
 {
-    case DOCTOR = 0;
-    case SECRETARY = 1;
-    case ADMIN = 2;
+    return $useRole->value == UserRoles::DOCTOR->value;
+}
+public static  function isSecretary( UserRoles $useRole)
+{
+    return $useRole->value == UserRoles::SECRETARY->value;
+}
 
-    public static function values(): array
-    {
-        return array_column(self::cases(), 'value', 'name');
-    }
-
-    public static function isDoctor(UserRoles $userRole): bool
-    {
-        return $userRole === self::DOCTOR;
-    }
-
-    public static function isSecretary(UserRoles $userRole): bool
-    {
-        return $userRole === self::SECRETARY;
-    }
-
-    public static function isAdmin(UserRoles $userRole): bool
-    {
-        return $userRole === self::ADMIN;
-    }
+public static  function isAdmin( UserRoles $useRole)
+{
+    return $useRole->value == UserRoles::ADMIN->value;
+}
 }
